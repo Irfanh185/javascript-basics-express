28832805c312e998a92dde199fdabf3ef31bb536
Irfan Hussain <irfan185@gmail.com>
2022-08-17 11:13:04 +0100

    strings

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
+
+app.get('/strings/hello/world', (req, res) => {
+  res.status(200).json({ result: 'Hello, world!' });
+});
 
 module.exports = app;
